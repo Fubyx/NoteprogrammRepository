e11dea9f9a31859d165b2ed1abc9da525ebf5360
miguel <stgasmig@bx.fallmerayer.it>
2023-04-28 10:33:01 +0200

    28.04.2023 Miguel: Testing addCalenderEntry, Testing removeCalendarEntry, Testing editCalendarEntry

---
+++ src/main/test/project/notizprogrammrepository/model/Types/segments/CaledndarSegmentTest.java
@@ -0,0 +1,56 @@
+package project.notizprogrammrepository.model.Types.segments;
+
+import org.junit.jupiter.api.Test;
+import project.notizprogrammrepository.model.Types.entries.CalendarEntry;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+//28.04.2023 Miguel: Testing addCalenderEntry, Testing removeCalendarEntry, Testing editCalendarEntry
+public class CaledndarSegmentTest {
+
+    @Test
+    public void addCalendarEntryTest(){
+        String title = "Tittel";
+        Date date = new Date();
+        CalendarEntry calendarEntry = new CalendarEntry(title, date);
+        CalendarSegment calendarSegment = new CalendarSegment(date);
+        calendarSegment.addCalendarEntry(calendarEntry);
+
+        assertEquals(calendarEntry, calendarSegment.getCalendarEntry(date));
+    }
+
+    @Test
+    public void removeCalendarEntryTest(){
+        String title = "TestName";
+        Date date = new Date();
+        CalendarEntry calendarEntry = new CalendarEntry(title, date);
+        CalendarSegment calendarSegment = new CalendarSegment(date);
+        calendarSegment.addCalendarEntry(calendarEntry);
+
+        calendarSegment.removeCalendarEntry(calendarEntry);
+        assertNull(calendarSegment.getCalendarEntry(date));
+    }
+
+    @Test
+    public void editCalenderEntryTest(){
+        String title = "titel1";
+        Date date = new Date();
+        CalendarEntry oldcalendarEntry = new CalendarEntry(title, date);
+
+        CalendarSegment calendarSegment = new CalendarSegment(date);
+        calendarSegment.addCalendarEntry(oldcalendarEntry);
+
+        String newtitle = "title2";
+        CalendarEntry newcalendarEntry = new CalendarEntry(newtitle, date);
+
+        calendarSegment.editCalendarEntry(newcalendarEntry);
+
+        assertEquals(newcalendarEntry.getTitle(), calendarSegment.getCalendarEntry(date).getTitle());
+        assertEquals(newcalendarEntry.getDate(), calendarSegment.getCalendarEntry(date).getDate());
+
+    }
+}
+
