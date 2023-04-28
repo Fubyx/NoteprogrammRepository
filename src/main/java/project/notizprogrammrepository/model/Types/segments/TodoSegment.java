b990ca433371f64191511458a5cf5fd13ba03cf3
Fubyx <fabireif11@gmail.com>
2023-04-28 11:06:11 +0200

    TodoSegment done

--- src/main/java/project/notizprogrammrepository/model/Types/segments/TodoSegment.java
+++ src/main/java/project/notizprogrammrepository/model/Types/segments/TodoSegment.java
@@ -6,11 +6,10 @@ import project.notizprogrammrepository.model.Types.entries.TodoEntry;
 
 import java.util.*;
 
-//28.04.2023 Fabian: editEntry
+//28.04.2023 Fabian: add-, remove-, editEntry
 public class TodoSegment extends Segment{
     private int nextEmptyId = Integer.MIN_VALUE;
-    private PriorityQueue<TodoEntry> entries =
-            new PriorityQueue<>((todoEntry1, todoEntry2) -> Integer.compare(todoEntry1.getPriority(), todoEntry2.getPriority()));
+    private ArrayList<TodoEntry> entries = new ArrayList<>();
 
     public void addEntry(Entry entry) {
         TodoEntry t = (TodoEntry)entry;
@@ -18,32 +17,38 @@ public class TodoSegment extends Segment{
         ++nextEmptyId;
         entries.add(t);
     }
-
     public void removeEntry(Entry entry) {
         entries.remove((TodoEntry) entry);
     }
-
     public void editEntry(Entry entry) {
         TodoEntry oldEntry = getEntry(((TodoEntry)entry).getId());
         if(oldEntry == null)
             return;
-        oldEntry.setTitle(entry.getTitle());
-        oldEntry.setText(entry.getText());
-        oldEntry.setDate(entry.getDate());
-        oldEntry.setPriority(((TodoEntry)entry).getPriority());
+        try {
+            oldEntry.setTitle(entry.getTitle());
+        }catch (NullPointerException n){}
+        try {
+            oldEntry.setText(entry.getText());
+        }catch (NullPointerException n){}
+        try {
+            oldEntry.setDate(entry.getDate());
+        }catch (NullPointerException n){}
+        try {
+            oldEntry.setPriority(((TodoEntry)entry).getPriority());
+        }catch (NullPointerException n){}
     }
     private TodoEntry getEntry(long id){
          Iterator<TodoEntry> it = entries.iterator();
          while (it.hasNext()){
              TodoEntry next = it.next();
-             if(next.getDate().equals(id)){
+             if(next.getId() == id){
                  return next;
              }
          }
          return null;
     }
-
-    public PriorityQueue<TodoEntry> getEntries() {
+    public ArrayList<TodoEntry> getEntries() {
+        entries.sort((todoEntry1, todoEntry2) -> Integer.compare(todoEntry1.getPriority(), todoEntry2.getPriority()));
         return entries;
     }
 }
--- src/main/test/project/notizprogrammrepository/model/Types/segments/TodoSegmentTest.java
+++ src/main/test/project/notizprogrammrepository/model/Types/segments/TodoSegmentTest.java
@@ -3,6 +3,8 @@ package project.notizprogrammrepository.model.Types.segments;
 import org.junit.jupiter.api.Test;
 import project.notizprogrammrepository.model.Types.entries.TodoEntry;
 
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -12,10 +14,32 @@ public class TodoSegmentTest {
     public void addRemoveTest(){
         TodoSegment todoSegment = new TodoSegment();
         TodoEntry t = new TodoEntry("TestTitle");
-        assertNull(todoSegment.getEntries().peek());
+        assertEquals(0, todoSegment.getEntries().size());
         todoSegment.addEntry(t);
-        assertEquals(t, todoSegment.getEntries().peek());
-        assertEquals("TestTitle", todoSegment.getEntries().peek().getTitle());
-        t.setTitle("TTT");
+        assertEquals(t, todoSegment.getEntries().get(0));
+        assertEquals("TestTitle", todoSegment.getEntries().get(0).getTitle());
+        TodoEntry newT = new TodoEntry("TTT");
+        newT.setId(t.getId());
+        todoSegment.editEntry(newT);
+        assertEquals("TTT", todoSegment.getEntries().get(0).getTitle());
+        todoSegment.removeEntry(t);
+        assertEquals(0, todoSegment.getEntries().size());
+
+        TodoEntry t1 = new TodoEntry("Hallo", 1);
+        TodoEntry t2 = new TodoEntry("Hallllllooo", 3);
+        TodoEntry t3 = new TodoEntry("tzzwfd", 0);
+
+        todoSegment.addEntry(t1);
+        todoSegment.addEntry(t2);
+        assertEquals(t1, todoSegment.getEntries().get(0));
+
+        todoSegment.addEntry(t3);
+        assertEquals(t3, todoSegment.getEntries().get(0));
+        Iterator<TodoEntry> it = todoSegment.getEntries().iterator();
+        assertEquals(t3, it.next());
+        assertEquals(t1, it.next());
+        assertEquals(t2, it.next());
+
+
     }
 }
