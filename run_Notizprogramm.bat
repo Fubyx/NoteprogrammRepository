b3d097e609ede2e74781d7c5bfb39a95ac3849c2
Fubyx <fabireif11@gmail.com>
2023-05-20 19:51:25 +0200

    fixed pom ad created exe

---
+++ run_Notizprogramm.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD1VTg2DMleeA6YX/Ofr08WFp18OV+oqdIrnjpW6HMkd7Er2eJc533FmqNkDBh5Tai6CYAk7lVxHpmuWPtOToRbdZEed71EgCWpzlWLdgRk+cNx7k9cMwTmC0kLrlr8Aw3f4TKgDGC3k2akI
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+C:\Users\Fabian\Desktop\Oberschule\4AT\Informatik\Projekt\Info\Repository\NotizprogrammRepository\target\app\bin\java --module project.notizprogrammrepository/project.notizprogrammrepository.MainApplication
